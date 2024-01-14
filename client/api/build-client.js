665ac8d9cfb752e11f1160287677cd0aa4fe9685
Mark Morcos <mark.yehia@gmail.com>
2024-01-14 13:29:12 +0100

    chore: update domain

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://ticketing-app.xyz",
+      baseURL: "http://mark-ticketing-app.xyz",
       headers: req.headers,
     });
   }
