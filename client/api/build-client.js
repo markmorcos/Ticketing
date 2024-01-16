01b4c1c83f87f3c319bfcf5dab70ecf42a60badc
Mark Morcos <mark.yehia@gmail.com>
2024-01-16 08:51:48 +0100

    chore: revert host

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default ({ req }) => {
   if (typeof window === "undefined") {
     return axios.create({
-      baseURL: "http://159.223.249.202/",
+      baseURL: "http://mark-ticketing-app.xyz",
       headers: req.headers,
     });
   }
